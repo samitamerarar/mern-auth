d76d45f146ff65c86a00c960647ded4cd18fbd4a
Sami <sami.t.arar@hotmail.com>
2021-06-20 10:33:56 -0400

    backend login and register routes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
     "mongodb+srv://dbUser:dbUserPassword123@cluster0.ptx1e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secretOrKey: "secret",
 };
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+const passport = require("passport");
+const users = require("./routes/api/users");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -13,6 +16,15 @@ mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // Heroku Port
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
